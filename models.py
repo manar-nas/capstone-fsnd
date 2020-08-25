e7ed3b847f4e9b017e466cdea58085b3ede53599
منار عبدالرحمن <mnoo09165@gmail.com>
2020-08-25 10:29:07 +0300

    update

--- models.py
+++ models.py
@@ -5,29 +5,17 @@
 
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import os
+
 db = SQLAlchemy()
 
-database_name = "agency"
 
-''' database_path = "postgres://{}:{}@{}/{}".format(
-    'postgres', '5678.mna', 'localhost:5432', database_name)'''
+def setup_db(app, database_path=os.environ.get('DATABASE_URL')):
 
-database_path = os.environ['DATABASE_URL']
-'''
-db_setup(app)
-    binds a flask application and a SQLAlchemy service
-'''
-
-
-def db_setup(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    Migrate(app, db)
-    return db
 
 
 # ----------------------------------------------------------------------------#
